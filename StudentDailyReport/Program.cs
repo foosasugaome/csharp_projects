b5b90c5900caeae9d5868a985238f35db96f901c
Norman Teodoro <normanteodoro@hotmail.com>
2023-05-23 09:04:19 -0700

    commit for Daily Report Assignment

---
+++ StudentDailyReport/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StudentDailyReport
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Print the header
+            Console.WriteLine("Academy of Learning Career College");
+            Console.WriteLine("Student Daily Report");            
+
+            // Ask and save student data
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            int pageNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false'.");
+            bool needsHelp = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            double studyHours = double.Parse(Console.ReadLine());
+
+            // Print a thank you message
+            Console.WriteLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VariableAndDataTypes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableAndDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is " + yourName);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            Console.WriteLine(isStudying);
+            Console.ReadLine();
+
+            byte value = 255;
+            sbyte temperature = -10;
+            char letter = 'A';
+            decimal price = 9.99m;
+            double pi = 3.14159;
+            float distance = 3.5f;
+            int age = 25;
+            uint count = 100;
+            long population = 1000000L;
+            ulong total = 999999999UL;            
+            ushort quantity = 500;
+            string name = "John Doe";
+            object data = "Some data";
+
+        }
+    }
+}
