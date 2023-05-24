732f0511759e51fd038948702a3da0082664a0d6
Norman Teodoro <normanteodoro@hotmail.com>
2023-05-24 10:05:31 -0700

    added comments on code

--- PackageExpress/Program.cs
+++ PackageExpress/Program.cs
@@ -13,9 +13,11 @@ namespace PackageExpress
             Console.WriteLine("Enter package weight in kilograms.");
             double packageWeight = Convert.ToDouble(Console.ReadLine());
 
+            //Check if package  is over 50. 
             bool validWeight = packageWeight > 50 ? validWeight=false : validWeight = true;
 
-            if (validWeight)
+            
+            if (validWeight) //If package under 50
             {
                 Console.WriteLine("Enter package width");
                 double packageWidth = Convert.ToDouble(Console.ReadLine());
@@ -26,25 +28,24 @@ namespace PackageExpress
                 Console.WriteLine("Enter package length");
                 double packageLength = Convert.ToDouble(Console.ReadLine());
 
-                double dimension = packageWidth * packageHeight * packageLength;
-
+                // Check  if  dimension  is   over 50. Exit if one is over 50.
                 if (packageWidth > 50 || packageHeight > 50 || packageLength  > 50)
                 {
                     Console.WriteLine("Package too big to be shipped via Package Express.");
                 }
-                else
+                else //  Compute quote.
                 {
+                    double dimension = packageWidth * packageHeight * packageLength;
                     double quote = (dimension * packageWeight) / 100;
                     Console.WriteLine("Your estimated  total for shipping this package is : $" + quote.ToString("0.00")  +  "\nThank you!");
                 }
             }
             else
             {
+                //Exit if  package is too heavy.
                 Console.WriteLine("Package too Heave to  be shipped via Package Express. Have a good day.");
             }
             Console.ReadLine();
-
-
         }
     }
 }
