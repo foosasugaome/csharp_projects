316a462ed2b4e1159779810545d5d91b9096e5c1
Norman Teodoro <normanteodoro@hotmail.com>
2023-05-24 12:08:18 -0700

    Console App Assignment commit

---
+++ ConsoleAppAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boolean comparison using a while statement
+            Console.WriteLine("Boolean comparison using a while statement:");
+
+            int counter = 1; // Initialize a counter variable
+
+            while (counter <= 5) // Loop while the counter is less than or equal to 5
+            {
+                Console.WriteLine("Counter value: " + counter);
+                counter++; // Increment the counter by 1
+            }
+
+            Console.WriteLine();
+
+            // Boolean comparison using a do-while statement
+            Console.WriteLine("Boolean comparison using a do-while statement:");
+
+            int number;
+
+            do
+            {
+                Console.Write("Enter a number greater than 10: ");
+                number = int.Parse(Console.ReadLine());
+            } while (number <= 10); // Loop while the number is less than or equal to 10
+
+            Console.WriteLine("Number entered: " + number);
+
+            Console.ReadLine();
+        }    
+    }
+}
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -10,16 +10,46 @@ namespace Sandbox
     {
         static void Main()
         {
-            //Ternary Operator
-            Console.WriteLine("Enter a number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("Enter another number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            ////Ternary Operator
+            //Console.WriteLine("Enter a number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
 
-            string result = num1 > num2 ? "num1 is greater than num2" : "num2 is greater than num1";
-               
-            Console.WriteLine(result);
+            //Console.WriteLine("Enter another number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //string result = num1 > num2 ? "num1 is greater than num2" : "num2 is greater than num1";
+
+            //Console.WriteLine(result);
+
+
+            // Switch Challenge
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+
+            
             Console.ReadLine();
 
         }
