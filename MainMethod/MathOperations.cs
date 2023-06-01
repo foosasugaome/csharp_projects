955c59a94d2e488af863f430cc641339c2137838
Norman Teodoro <normanteodoro@hotmail.com>
2023-06-01 09:55:38 -0700

    commit main method

---
+++ MainMethod/MathOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class MathOperations
+    {
+        // Method that performs a math operation on an integer
+        public int PerformOperation(int number)
+        {
+            // Example math operation: square the input number
+            int result = number * number;
+            return result;
+        }
+
+        // Method that performs a different math operation on a decimal
+        public int PerformOperation(decimal number)
+        {
+            // Example math operation: calculate the square root of the input number
+            int result = (int)Math.Sqrt((double)number);
+            return result;
+        }
+
+        // Method that performs a different math operation on a string (if it can be converted to an integer)
+        public int PerformOperation(string input)
+        {
+            // Example math operation: calculate the length of the input string
+            int result = 0;
+
+            // Try to convert the input string to an integer
+            if (int.TryParse(input, out int number))
+            {
+                result = input.Length;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MainMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOperations = new MathOperations();
+
+            // Perform the operation on an integer
+            int intResult = mathOperations.PerformOperation(5);
+            Console.WriteLine("Result for integer: " + intResult);
+
+            // Perform the operation on a decimal
+            int decimalResult = mathOperations.PerformOperation(8.7m);
+            Console.WriteLine("Result for decimal: " + decimalResult);
+
+            // Perform the operation on a string
+            string input = "12345";
+            int stringResult = mathOperations.PerformOperation(input);
+            Console.WriteLine("Result for string: " + stringResult);
+
+            // Wait for the user to press a key before closing the console window
+            Console.ReadKey();
+        }
+    }
+}
