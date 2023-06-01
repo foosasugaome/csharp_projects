55273f75bb34ad5f34586a7bec001357797357b9
Norman Teodoro <normanteodoro@hotmail.com>
2023-06-01 09:22:07 -0700

    commit  Methods assignment

---
+++ Methods/MathOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class MathOperations
+    {
+        // Method to double the input number
+        public int DoubleNumber(int number)
+        {
+            return number * 2;
+        }
+
+        // Method to square the input number
+        public int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        // Method to calculate the factorial of the input number
+        public int CalculateFactorial(int number)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of MathOperations class
+            MathOperations mathOperations = new MathOperations();
+
+            // Ask the user for a number
+            Console.Write("Enter a number: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            // Call each method and display the returned value
+            int doubledNumber = mathOperations.DoubleNumber(userInput);
+            Console.WriteLine("Double of the number: " + doubledNumber);
+
+            int squaredNumber = mathOperations.SquareNumber(userInput);
+            Console.WriteLine("Square of the number: " + squaredNumber);
+
+            int factorial = mathOperations.CalculateFactorial(userInput);
+            Console.WriteLine("Factorial of the number: " + factorial);
+
+           
+            Console.ReadLine();
+        }
+
+    }
+}
