a3c033fb019e5f5e09a755b315a9b9768a726365
Norman Teodoro <normanteodoro@hotmail.com>
2023-06-01 10:08:33 -0700

    Method Class Assignment Submission

---
+++ MethodClass/MathOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class MathOperations
+    {
+        // Method that performs a math operation on the first integer and displays the second integer
+        public int PerformOperation(int number1, int number2)
+        {
+            // Example math operation: multiply the first number by 2
+            int result = number1 * 2;
+            return result;
+        }
+    }
+}
---
+++ MethodClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOperations = new MathOperations();
+
+            // Call the method in the class, passing in two numbers
+            int result =  mathOperations.PerformOperation(5, 10);
+            Console.WriteLine("Result : {0}", result);
+
+            // Call the method in the class, specifying the parameters by name
+            result =  mathOperations.PerformOperation(number1: 8, number2: 20);
+            Console.WriteLine("Result : {0}", result);
+
+            // Wait for the user to press a key before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
