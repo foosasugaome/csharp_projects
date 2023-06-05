f9facacd83433f2d5cc1a8776524e91ed79cff56
Norman Teodoro <normanteodoro@hotmail.com>
2023-06-05 14:16:14 -0700

    removed extra comments in Program.cs

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -7,16 +7,6 @@ using System.Threading.Tasks;
 
 namespace Polymorphism
 {
-//Perform these actions and create a console app that includes the following:
-
-//1. Create an interface called IQuittable and have it define a void method called Quit().
-
-//2. Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
-
-//3. Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.
-
-//4. Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
-
 
     internal class Program
     {
